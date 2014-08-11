d3988fcfcc8f2c65a3f6b08e5763d1990fa6b715
Jason Judge <jason.judge@consil.co.uk>
2014-08-11 16:33:41 +0100

    Added purchase completion class.

---
+++ src/Message/HostedPagesCompletePurchase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Omnipay\Helcim\Message;
+
+use Omnipay\Common\Exception\InvalidResponseException;
+
+/**
+* Helcim Hosted Page Complete Purchase Request
+*/
+class HostedPagesCompletePurchase() extends AbstractRequest
+{
+}
+
