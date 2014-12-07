4bfca07e471744be8ea7d28573dce529149cf4d4
Jason Judge <jason.judge@consil.co.uk>
2014-12-07 22:23:12 +0000

    Started JS

--- src/JSGateway.php
+++ src/JSGateway.php
@@ -7,9 +7,30 @@ namespace Omnipay\Helcim;
  */
 class JSGateway extends AbstractCommonGateway
 {
+    public function getName()
+    {
+        return 'Helcim.JS';
+    }
+
     /**
      * The API version.
      * Furture versions will have different names, e.g. JS2Gateway
      */
     const $version = '1';
+
+    /**
+     * For handling a purchase.
+     */
+    public function purchase(array $parameters = array())
+    {
+        return $this->createRequest('\Omnipay\Helcim\Message\JSPurchaseRequest', $parameters);
+    }
+
+    /**
+    * For the return path from the remote Hosted Page.
+    */
+    public function completePurchase(array $parameters = array())
+    {
+        return $this->createRequest('\Omnipay\Helcim\Message\JSCompletePurchaseRequest', $parameters);
+    }
 }
